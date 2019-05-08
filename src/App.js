42107f47544d5098979ae330e49a24e85b8ee4d8
Baktijar Anapijaev <Baktiar2794@gmail.com>
2019-05-08 15:19:45 +0600

    imported pages to App js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
+import "./App.css";
 
-//import './App.css';
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
 
 function App() {
   return <>hello from App</>;
---
+++ src/pages/Error.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Error() {
+  return <div> hello from the Page is not found</div>;
+}
