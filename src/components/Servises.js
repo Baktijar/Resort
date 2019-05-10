764e367d4d6fe18e6c3e557d51d89e2cb55c2c15
Baktijar Anapijaev <Baktiar2794@gmail.com>
2019-05-10 13:18:12 +0600

    created componets Servises and Title and imported to home page

---
+++ src/components/Servises.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Title from "./Title";
+export default class Servises extends Component {
+  render() {
+    return (
+      <div>
+        <Title title="servises" />
+      </div>
+    );
+  }
+}
