7c97c3e602f6760245303d4780ef741c315695fe
Baktijar Anapijaev <Baktiar2794@gmail.com>
2019-05-10 13:47:31 +0600

    used.map to display servise icons

--- src/components/Servises.js
+++ src/components/Servises.js
@@ -2,11 +2,50 @@ import React, { Component } from "react";
 import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
 import Title from "./Title";
 export default class Servises extends Component {
+  state = {
+    servises: [
+      {
+        icon: <FaCocktail />,
+        title: "free cocktails",
+        info:
+          "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem facilis sint alias blanditiis adipisci! "
+      },
+      {
+        icon: <FaHiking />,
+        title: "Endless hiking",
+        info:
+          "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem facilis sint alias blanditiis adipisci! "
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "Free shuttle",
+        info:
+          "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem facilis sint alias blanditiis adipisci! "
+      },
+      {
+        icon: <FaBeer />,
+        title: "Strongest Beer",
+        info:
+          "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem facilis sint alias blanditiis adipisci! "
+      }
+    ]
+  };
   render() {
     return (
-      <div>
+      <section className="servises">
         <Title title="servises" />
-      </div>
+        <div className="servises-center">
+          {this.state.servises.map((item, index) => {
+            return (
+              <article key={index} className="service">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
     );
   }
 }
