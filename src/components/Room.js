c8a8dac8128003cdc30253e19a4c097a69148b8d
Baktijar Anapijaev <Baktiar2794@gmail.com>
2019-05-16 15:43:50 +0600

    added price

--- src/components/Room.js
+++ src/components/Room.js
@@ -6,7 +6,11 @@ export default function Room({ room }) {
   return (
     <article className="room">
       <div className="img-container">
-        <img src={images[0] ||defaultImg } alt="single room" />
+        <img src={images[0] || defaultImg} alt="single room" />
+        <div className="price-top">
+          <h6>${price}</h6>
+          <p>per night</p>
+        </div>
       </div>
     </article>
   );
