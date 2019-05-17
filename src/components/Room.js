cbccd465455f45425f969885f2f77f61c8eba0cd
Baktijar Anapijaev <Baktiar2794@gmail.com>
2019-05-17 11:51:42 +0600

    acsess params

--- src/components/Room.js
+++ src/components/Room.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import defaultImg from "../images/room-1.jpeg";
 import PropTypes from "prop-types";
-export default function Room({ room }) {
+import { memo } from "react";
+const Room = memo(({ room }) => {
   const { name, slug, images, price } = room;
+  // console.log(name);
   return (
     <article className="room">
       <div className="img-container">
@@ -12,15 +14,14 @@ export default function Room({ room }) {
           <h6>${price}</h6>
           <p>per night</p>
         </div>
-        <Link to={"/rooms/slug"} className="btn-primary room-link">
-          Features
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          features
         </Link>
       </div>
-
       <p className="room-info">{name}</p>
     </article>
   );
-}
+});
 
 Room.propTypes = {
   room: PropTypes.shape({
@@ -30,3 +31,4 @@ Room.propTypes = {
     price: PropTypes.number.isRequired
   })
 };
+export default Room;
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -6,6 +6,12 @@ import { Link } from "react-router-dom";
 import { RoomContext } from "../context";
 
 export default class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+  }
+  componentDidMount() {}
+
   render() {
     return <div>Hello from SingleRoom page</div>;
   }
