6848256a45fbfca5d063ea2775f121ae558d90a3
Baktijar Anapijaev <Baktiar2794@gmail.com>
2019-05-17 15:28:01 +0600

    adding singleRoom component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -9,10 +9,50 @@ export default class SingleRoom extends Component {
   constructor(props) {
     super(props);
     console.log(this.props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg: defaultBcg
+    };
   }
-  componentDidMount() {}
+  static contextType = RoomContext;
 
+  // componentDidMount() {
+  //   console.log(this.props);
+  // }
   render() {
-    return <div>Hello from SingleRoom page</div>;
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+
+    if (!room) {
+      return (
+        <div className="error">
+          <h3> no such room could be found...</h3>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </div>
+      );
+    }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images
+    } = room;
+
+    return (
+      <Hero hero="roomsHero">
+        <Banner title={`${name} room`}>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </Banner>
+      </Hero>
+    );
   }
 }
