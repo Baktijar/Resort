4836a207e3177e4c820f17633a221f9febb59f70
Baktijar Anapijaev <Baktiar2794@gmail.com>
2019-05-22 15:23:59 +0600

    start working with Room components

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RoomFilter from "./RoomFilter";
+import RoomList from "./RoomList";
+export default function RoomContainer() {
+  return (
+    <div>
+      helllo from RoomContainer
+      <RoomFilter />
+      <RoomList />
+    </div>
+  );
+}
