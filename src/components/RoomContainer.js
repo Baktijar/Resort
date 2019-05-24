478cb865efda07be4750f639d8cdc02c9e230d81
Baktijar Anapijaev <Baktiar2794@gmail.com>
2019-05-24 14:02:47 +0600

    using consumer to access the context

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,12 +1,26 @@
 import React from "react";
 import RoomFilter from "./RoomFilter";
 import RoomList from "./RoomList";
+import { RoomConsumer } from "../context";
+import Loading from "./Loading";
+
 export default function RoomContainer() {
   return (
-    <div>
-      helllo from RoomContainer
-      <RoomFilter />
-      <RoomList />
-    </div>
+    <RoomConsumer>
+      {value => {
+        const { Loading, sortedRooms, rooms } = value;
+        if (Loading) {
+          return <Loading />;
+        }
+
+        return (
+          <div>
+            hello from RoomContainer
+            <RoomFilter rooms={rooms} />
+            <RoomList rooms={sortedRooms} />
+          </div>
+        );
+      }}
+    </RoomConsumer>
   );
 }
