0c01a677979ce2add04f54150ca9e88b91932bcd
Baktijar Anapijaev <Baktiar2794@gmail.com>
2019-05-29 17:08:36 +0600

    started working with filter container

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,5 +1,25 @@
 import React from "react";
-
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "../components/Title";
 export default function RoomsFilter() {
-  return <div>helllo from RoomsFilter</div>;
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+    </section>
+  );
 }
