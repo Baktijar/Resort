51094f23363bf9d5fc6237a0fd73cdbded201190
Baktijar Anapijaev <Baktiar2794@gmail.com>
2019-05-31 00:06:10 +0600

    filter by extras

--- src/context.js
+++ src/context.js
@@ -55,7 +55,7 @@ class RoomProvider extends Component {
 
   handleChange = event => {
     const target = event.target;
-    const value = event.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
     this.setState(
       {
@@ -91,9 +91,19 @@ class RoomProvider extends Component {
     }
     //filter by price
     tempRooms = tempRooms.filter(room => room.price <= price);
+    //filter by size
     tempRooms = tempRooms.filter(
       room => room.size >= minSize && room.size <= maxSize
     );
+    // filter by breakfast
+    if (breakFast) {
+      tempRooms = tempRooms.filter(room => room.breakFast === true);
+    }
+    // filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true);
+    }
+
     //change state
     this.setState({
       sortedRooms: tempRooms
